6a7dbf5741c62905faa94d0cb1298b8c0da8fac8
Daniel <klee0589@gmail.com>
2023-06-18 23:19:20 -0400

    added axios

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react";
+
 // import logo from "./logo.svg";
 import "./App.css";
 import Button from "@mui/material/Button";
@@ -5,15 +7,53 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./features/combos/comboSlice";
+
+import axios from "axios";
+
 function App() {
+  const count = useSelector((state) => state.combo);
+  const dispatch = useDispatch();
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/users`).then((response) => {
+      const users = response.data;
+      setData({ users });
+    });
+  }, []);
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
   return (
     <div className="App">
       <Card sx={{ minWidth: 275 }}>
         <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-          <Button variant="contained">Hello World</Button>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Added
+          </Typography>
+          {/* {data} */}
+          <div>
+            <Button
+              variant="contained"
+              aria-label="Increment value"
+              onClick={() => dispatch(increment())}
+            >
+              Increment
+            </Button>
+            <span>{count}</span>
+            <Button
+              variant="contained"
+              aria-label="Decrement value"
+              onClick={() => dispatch(decrement())}
+            >
+              Decrement
+            </Button>
+          </div>
         </CardContent>
       </Card>
     </div>
