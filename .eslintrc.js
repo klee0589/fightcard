1839db25a427c1794c7d17d6e67c3b217afb0358
Daniel <klee0589@gmail.com>
2023-07-09 17:33:37 -0400

    added storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  root: true, 
+  root: true,
   parserOptions: {
-    ecmaVersion: 2020, 
-    sourceType: 'module', 
+    ecmaVersion: 2020,
+    sourceType: 'module',
     ecmaFeatures: {
-      jsx: true 
+      jsx: true
     }
   },
   settings: {
@@ -14,17 +14,19 @@ module.exports = {
   },
   env: {
     jest: true,
-    browser: true, 
-    amd: true, 
-    node: true 
+    browser: true,
+    amd: true,
+    node: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended' // Make this the last element so prettier config overrides other formatting rules
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
   rules: {
-    'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
-    'prettier/prettier': ['error', {}, { usePrettierrc: true }] 
+    'no-unused-vars': ['error', {
+      vars: 'all',
+      args: 'after-used',
+      ignoreRestSiblings: false
+    }],
+    'prettier/prettier': ['error', {}, {
+      usePrettierrc: true
+    }]
   }
-}
+};
