dc38e2271250dcdb868a227babf5be92975b53e0
Daniel <klee0589@gmail.com>
2023-07-16 15:32:40 -0400

    initial setup

---
+++ src/ui/ControlBar/ControlBar.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+// import { useSelector } from 'react-redux'
+// import { useFela } from 'react-fela'
+
+// import Button from '@mui/material/Button'
+
+// import { TComboType } from './types'
+
+// import { mainContainer, cardContainer, comboName, timer, buttonContainer, button } from './styles'
+
+const ControlBar = () => {
+  //   const { css } = useFela()
+  //   const combos = useSelector((state: any) => state.combo)
+
+  //   const [isDrilling, setIsDrilling] = useState<boolean>(false)
+
+  //   useEffect(() => {
+  //     const msg = new SpeechSynthesisUtterance()
+  //     if (combo) {
+  //       const splitCombo = combo.name.split('_')
+  //       msg.text = 'Combo ' + splitCombo[1]
+  //       msg.volume = 0.7
+  //       isDrilling && window.speechSynthesis.speak(msg)
+  //     }
+  //   }, [combo, isDrilling])
+
+  return (
+    // <div className={css(mainContainer(isDrilling))}>
+    <div>Control Bar</div>
+  )
+}
+
+export default ControlBar
